1d9d3da21e302734b56c7a342a905312a5e14d37
Nahom Habtamu <nahomhab2626@gmail.com>
2023-05-24 19:00:54 +0300

    feature: add middleware configurations

--- routes/api.php
+++ routes/api.php
@@ -4,28 +4,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SupplierController;
  
-Route::get('suppliers', 'App\Http\Controllers\SupplierController@index');
-Route::get('suppliers/{id}', 'App\Http\Controllers\SupplierController@show');
-Route::post('suppliers', 'App\Http\Controllers\SupplierController@store');
-Route::put('suppliers/{id}', 'App\Http\Controllers\SupplierController@update');
-Route::delete('suppliers/{id}', 'App\Http\Controllers\SupplierController@delete');
-
-Route::get('warehouses', 'App\Http\Controllers\WarehouseController@index');
-Route::get('warehouses/{id}', 'App\Http\Controllers\WarehouseController@show');
-Route::post('warehouses', 'App\Http\Controllers\WarehouseController@store');
-Route::put('warehouses/{id}', 'App\Http\Controllers\WarehouseController@update');
-Route::delete('warehouses/{id}', 'App\Http\Controllers\WarehouseController@delete');
-
-Route::get('products', 'App\Http\Controllers\ProductController@index');
-Route::get('products/{id}', 'App\Http\Controllers\ProductController@show');
-Route::post('products', 'App\Http\Controllers\ProductController@store');
-Route::put('products/{id}', 'App\Http\Controllers\ProductController@update');
-Route::delete('products/{id}', 'App\Http\Controllers\ProductController@delete');
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('suppliers', 'App\Http\Controllers\SupplierController@index');
+    Route::get('suppliers/{id}', 'App\Http\Controllers\SupplierController@show');
+    Route::post('suppliers', 'App\Http\Controllers\SupplierController@store');
+    Route::put('suppliers/{id}', 'App\Http\Controllers\SupplierController@update');
+    Route::delete('suppliers/{id}', 'App\Http\Controllers\SupplierController@delete');
+    
+    Route::get('warehouses', 'App\Http\Controllers\WarehouseController@index');
+    Route::get('warehouses/{id}', 'App\Http\Controllers\WarehouseController@show');
+    Route::post('warehouses', 'App\Http\Controllers\WarehouseController@store');
+    Route::put('warehouses/{id}', 'App\Http\Controllers\WarehouseController@update');
+    Route::delete('warehouses/{id}', 'App\Http\Controllers\WarehouseController@delete');
+    
+    Route::get('products', 'App\Http\Controllers\ProductController@index');
+    Route::get('products/{id}', 'App\Http\Controllers\ProductController@show');
+    Route::post('products', 'App\Http\Controllers\ProductController@store');
+    Route::put('products/{id}', 'App\Http\Controllers\ProductController@update');
+    Route::delete('products/{id}', 'App\Http\Controllers\ProductController@delete');
+});
 
 Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
 Route::post('logout', 'App\Http\Controllers\Auth\LoginController@logout');
-
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
