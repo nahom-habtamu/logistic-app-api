3575abff72a3220361547160bea5b1de5212f8b1
Nahom Habtamu <nahomhab2626@gmail.com>
2023-05-26 11:28:29 +0300

    refactor: use the resources method to shorten and clean registration of routes

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SupplierController;
- 
+use App\Http\Controllers\ProductController;
+
 Route::group(['middleware' => 'auth:api'], function() {
-    Route::get('suppliers', 'App\Http\Controllers\SupplierController@index');
-    Route::get('suppliers/{id}', 'App\Http\Controllers\SupplierController@show');
-    Route::post('suppliers', 'App\Http\Controllers\SupplierController@store');
-    Route::put('suppliers/{id}', 'App\Http\Controllers\SupplierController@update');
-    Route::delete('suppliers/{id}', 'App\Http\Controllers\SupplierController@delete');
-    
-    Route::get('warehouses', 'App\Http\Controllers\WarehouseController@index');
-    Route::get('warehouses/{id}', 'App\Http\Controllers\WarehouseController@show');
-    Route::post('warehouses', 'App\Http\Controllers\WarehouseController@store');
-    Route::put('warehouses/{id}', 'App\Http\Controllers\WarehouseController@update');
-    Route::delete('warehouses/{id}', 'App\Http\Controllers\WarehouseController@delete');
-    
-    Route::get('products', 'App\Http\Controllers\ProductController@index');
-    Route::get('products/{id}', 'App\Http\Controllers\ProductController@show');
-    Route::post('products', 'App\Http\Controllers\ProductController@store');
-    Route::put('products/{id}', 'App\Http\Controllers\ProductController@update');
-    Route::delete('products/{id}', 'App\Http\Controllers\ProductController@delete');
+    Route::resources([
+        'suppliers' => SupplierController::class,
+        'warehouses' => WarehouseController::class,
+        'products' => ProductController::class,
+    ]);
 });
 
 Route::post('login', 'App\Http\Controllers\Auth\LoginController@login');
